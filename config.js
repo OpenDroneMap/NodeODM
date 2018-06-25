a479b5de19baa58779ef01908f81a486389e2e32
Piero Toffanin <pt@masseranolabs.com>
2018-06-25 11:42:44 -0400

    Spaces to tabs

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ if (argv.help){
 Usage: node index.js [options]
 
 Options:
-	--config	<path>	Path to the configuration file (default: config-default.json)	
+	--config <path>	Path to the configuration file (default: config-default.json)	
 	-p, --port <number> 	Port to bind the server to (default: 3000)
 	--odm_path <path>	Path to OpenDroneMap's code	(default: /code)
 	--log_level <logLevel>	Set log level verbosity (default: info)
@@ -36,8 +36,8 @@ Options:
 	--test Enable test mode. In test mode, no commands are sent to OpenDroneMap. This can be useful during development or testing (default: false)
 	--test_skip_orthophotos	If test mode is enabled, skip orthophoto results when generating assets. (default: false) 
 	--test_skip_dems	If test mode is enabled, skip dems results when generating assets. (default: false) 
-    --powercycle	When set, the application exits immediately after powering up. Useful for testing launch and compilation issues.
-    --token     <token>     Sets a token that needs to be passed for every request. This can be used to limit access to the node only to token holders. (default: none)
+	--powercycle	When set, the application exits immediately after powering up. Useful for testing launch and compilation issues.
+	--token <token>	Sets a token that needs to be passed for every request. This can be used to limit access to the node only to token holders. (default: none)
 Log Levels: 
 error | debug | info | verbose | debug | silly 
 `);
