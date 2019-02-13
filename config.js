1ec908e9d61bd08c49739b12d1722ff6b79757e1
Piero Toffanin <pt@masseranolabs.com>
2019-02-13 14:03:28 -0500

    Added ability to test task fails

--- config.js
+++ config.js
@@ -38,6 +38,7 @@ Options:
 	--test_skip_orthophotos	If test mode is enabled, skip orthophoto results when generating assets. (default: false) 
 	--test_skip_dems	If test mode is enabled, skip dems results when generating assets. (default: false) 
 	--test_drop_uploads	If test mode is enabled, drop /task/new/upload requests with 50% probability. (default: false)
+	--test_fail_tasks	If test mode is enabled, mark tasks as failed. (default: false)
 	--powercycle	When set, the application exits immediately after powering up. Useful for testing launch and compilation issues.
 	--token <token>	Sets a token that needs to be passed for every request. This can be used to limit access to the node only to token holders. (default: none)
 	--max_images <number>	Specify the maximum number of images that this processing node supports. (default: unlimited)
@@ -97,6 +98,7 @@ config.test = argv.test || fromConfigFile("test", false);
 config.testSkipOrthophotos = argv.test_skip_orthophotos || fromConfigFile("testSkipOrthophotos", false);
 config.testSkipDems = argv.test_skip_dems || fromConfigFile("testSkipDems", false);
 config.testDropUploads = argv.test_drop_uploads || fromConfigFile("testDropUploads", false);
+config.testFailTasks = argv.test_fail_tasks || fromConfigFile("testFailTasks", false);
 config.powercycle = argv.powercycle || fromConfigFile("powercycle", false);
 config.token = argv.token || fromConfigFile("token", "");
 config.maxImages = parseInt(argv.max_images || fromConfigFile("maxImages", "")) || null;
