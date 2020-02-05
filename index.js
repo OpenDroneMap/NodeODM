df7dc5a9cd09f23f73a6b45cacd5259234d0b9ce
Piero Toffanin <pt@masseranolabs.com>
2020-02-05 09:18:42 -0500

    Revert zipping to legacy

--- index.js
+++ index.js
@@ -901,9 +901,9 @@ if (config.test) {
     if (config.testDropUploads) logger.info("Uploads will drop at random");
 }
 
-if (!config.has7z){
-    logger.warn("The 7z program is not installed, falling back to legacy (zipping will be slower)");
-}
+// if (!config.has7z){
+//     logger.warn("The 7z program is not installed, falling back to legacy (zipping will be slower)");
+// }
 
 let commands = [
     cb => odmInfo.initialize(cb),
