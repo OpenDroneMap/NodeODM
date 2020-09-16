aa2347ef3b1c5eed72013feeb5d93cc233aa821f
Bibek Joshi <42735993+bibekjoshisrijan@users.noreply.github.com>
2020-09-17 00:09:01 +0530

    Added the correct value from config file

--- libs/S3.js
+++ libs/S3.js
@@ -76,7 +76,7 @@ module.exports = {
                 Bucket: bucket,
                 Key: file.dest,
                 Body: fs.createReadStream(file.src),
-                ACL: config.s3_acl
+                ACL: config.s3ACL
             }, {partSize: 5 * 1024 * 1024, queueSize: 1}, err => {
                 if (err){
                     logger.debug(err);
