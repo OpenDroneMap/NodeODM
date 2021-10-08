a14d8564b1ceb12708b016c87c63cdf5580d4808
Branton Davis <bluecamel@gmail.com>
2021-10-07 21:58:07 -0500

    Update run script to include environment.

--- run_gpu_intel.sh
+++ run_gpu_intel.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "${RENDER_GROUP_ID}" -ne 0 ]; then
-    groupadd -g "${RENDER_GROUP_ID}" render
-    usermod -aG render odm
+if [ ! -z "${RENDER_GROUP_ID}" ]; then
+    if [ "${RENDER_GROUP_ID}" -ne 0 ]; then
+        groupadd -g "${RENDER_GROUP_ID}" render
+        usermod -aG render odm
+    fi
 fi
 
-su - odm -c "/usr/bin/node /var/www/index.js $@"
+while IFS='=' read -r name value ; do
+    echo "export ${name}=\"${value}\"" >> /home/odm/env
+done < <(env)
+chown odm:odm /home/odm/env
+
+su - odm -c "source /home/odm/env; cd /var/www; echo $WO_DEFAULT_NODES; /usr/bin/node /var/www/index.js $@"
