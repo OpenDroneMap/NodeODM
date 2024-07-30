cf7754cd31c7155c9d0121994c1c0075da719b84
Frank Marazita <fmarazita1@gmail.com>
2024-07-30 14:27:08 +1000

    fix: support v6 and v7 uuids

--- libs/taskNew.js
+++ libs/taskNew.js
@@ -99,7 +99,7 @@ module.exports = {
             const userUuid = req.get('set-uuid');
     
             // Valid UUID and no other task with same UUID?
-            if (/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(userUuid) && !TaskManager.singleton().find(userUuid)){
+            if (/^[0-9a-f]{8}-[0-9a-f]{4}-[1-7][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(userUuid) && !TaskManager.singleton().find(userUuid)){
                 req.id = userUuid;
                 next();
             }else{
